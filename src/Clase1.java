a1f5a48157220af6aa15eedd3f50871d8e611f16
CarlosProfeGitHub <csanchis1@hotmail.com>
2022-03-09 19:28:40 +0100

    mensaje adiós

--- src/Clase1.java
+++ src/Clase1.java
@@ -4,6 +4,7 @@ public class Clase1 {
 	public static void main(String[] args) {
 		// FIXME Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Adiós mundo");
 
 	}
 
