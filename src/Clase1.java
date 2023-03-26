58eb42196bb8b9f60ffa98f6ccfe4bdfa3eb24c0
itzna <itzna@DESKTOP-HLEUSS6>
2023-03-26 20:50:24 +0200

    Mi Primera versión

--- src/Clase1.java
+++ src/Clase1.java
@@ -4,9 +4,7 @@ public class Clase1 {
 	public static void main(String[] args) {
 		// FIXME Auto-generated method stub
 		System.out.println("Hola mundo");
-		System.out.println("Adiós mundo");
-		System.out.println("Hola de nuevo");
-
+		System.out.println("Ivan Usina Aguayo");
 	}
 
 }
